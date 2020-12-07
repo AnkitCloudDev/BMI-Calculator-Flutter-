d5decaee2cb6a4c3ecec68348034e72ece95d037
AnkitCloudDev <ankitdongre96@gmail.com>
2020-12-07 09:48:44 +0000

    Missing Imports Fixed

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/Icon_Contect.dart';
 import 'package:bmi_calculator/input_page.dart';
+import 'package:bmi_calculator/widgets/build_gesture.dart';
 
 import 'package:flutter/material.dart';
 
